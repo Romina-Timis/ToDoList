9195005346ce5a50866e13d7d2a70e856696520e
Danielearosio <danielearosio95@gmail.com>
2025-04-01 15:16:23 +0200

    Add method to retrieve all tasks in TaskController and handle empty task list

--- src/main/java/com/projectwork/todolist/controller/TaskController.java
+++ src/main/java/com/projectwork/todolist/controller/TaskController.java
@@ -1,12 +1,17 @@
 package com.projectwork.todolist.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.projectwork.todolist.service.TaskService;
 
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.projectwork.todolist.model.Task;
 
 @Controller
 @RequestMapping("/")
@@ -15,6 +20,15 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
-   
-    
+    @GetMapping("/")
+    public String getAllTask(Model model) {
+        List<Task> task = taskService.getAllTask();
+        if (task == null || task.isEmpty()) {
+            model.addAttribute("errorMessage", "Nessun task disponibile al momento.");
+        } else {
+            model.addAttribute("task", task);
+        }
+        return "index";
+    }
+
 }
