1a186c29652d640d565955c183c3d2a2ebedaf3c
romina <rominatimis098@gmail.com>
2025-04-01 12:05:38 +0200

    UserRepository e UserService

--- src/main/java/com/projectwork/todolist/service/UserService.java
+++ src/main/java/com/projectwork/todolist/service/UserService.java
@@ -1,10 +1,35 @@
 package com.projectwork.todolist.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projectwork.todolist.model.User;
+import com.projectwork.todolist.repository.UserRepository;
+
+@Service
 public class UserService {
 
-    public Object getAllUsers() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAllUsers'");
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
+
+    public Optional<User> getLibroById(int id) {
+        return userRepository.findById(id);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteLibro(int id) {
+        userRepository.deleteById(id);
+    }
+
     
 }
