314283392a73442f9d0b8d869666fa5d88c1beae
romina <rominatimis098@gmail.com>
2025-04-01 11:45:29 +0200

    User Update

--- src/main/java/com/projectwork/todolist/model/user.java
+++ src/main/java/com/projectwork/todolist/model/user.java
@@ -1,52 +1,31 @@
 package com.projectwork.todolist.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data; // Ensure Lombok is correctly imported
 
+@Data
 @Entity
 @Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "id")
+    private int id;
 
+    @Column(name = "nome", nullable = false) // Add missing comma
     private String nome;
+
+    @Column(name = "email", nullable = false) // Add missing comma
     private String email;
+    
+    @Column(name = "password", nullable = false) // Add missing comma
     private String password;
-    public User(Long id, String nome, String email, String password) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.password = password;
-    }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     
 
