173120e74e16283c91a34a9398b1c0d2207b43bb
Danielearosio <danielearosio95@gmail.com>
2025-04-01 11:47:18 +0200

    add generated value strategy for Task ID

--- src/main/java/com/projectwork/todolist/model/Task.java
+++ src/main/java/com/projectwork/todolist/model/Task.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -17,6 +19,7 @@ import lombok.Data;
 public class Task {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
