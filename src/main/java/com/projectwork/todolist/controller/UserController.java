b801a5fccd54833da0c699f57df97722372ee761
romina <rominatimis098@gmail.com>
2025-04-01 11:57:31 +0200

    ControllerUser

--- src/main/java/com/projectwork/todolist/controller/UserController.java
+++ src/main/java/com/projectwork/todolist/controller/UserController.java
@@ -1,5 +1,33 @@
 package com.projectwork.todolist.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projectwork.todolist.model.User;
+import com.projectwork.todolist.service.UserService;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
     
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    
+
 }
---
+++ src/main/java/com/projectwork/todolist/service/UserService.java
@@ -0,0 +1,10 @@
+package com.projectwork.todolist.service;
+
+public class UserService {
+
+    public Object getAllUsers() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllUsers'");
+    }
+    
+}
