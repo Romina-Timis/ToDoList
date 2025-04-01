d9245eabaf695dc6fd1196c2e3e405f3229eb26b
Danielearosio <danielearosio95@gmail.com>
2025-04-01 11:09:22 +0200

    add Task model class with attributes and status enumeration

---
+++ src/main/java/com/projectwork/todolist/model/Task.java
@@ -0,0 +1,44 @@
+package com.projectwork.todolist.model;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private Status status;
+
+    @Column(name = "due_date")
+    private Date dueDate;
+
+    @Column(name = "created_at", updatable = false)
+    private Timestamp createdAt;
+
+    public enum Status {
+        TODO,
+        IN_PROGRESS,
+        DONE
+    }
+}
