533738f76bcd718dee01333b334d10bfbd4a0b75
Danielearosio <danielearosio95@gmail.com>
2025-04-04 12:19:15 +0200

    Remove unused PasswordEncoder import from AdmUserService

--- src/main/java/com/projectwork/todolist/service/AdmUserService.java
+++ src/main/java/com/projectwork/todolist/service/AdmUserService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.projectwork.todolist.model.AdmUser;
 import com.projectwork.todolist.repository.AdmUserRepository;
